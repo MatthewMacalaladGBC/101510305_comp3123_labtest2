8a20544f272917e943732f92c6e142157058db1f
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2025-11-27 14:20:22 -0500

    initial test change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <p>COMP 3123, Lab Test 2</p>
+      <p>Matthew Macalalad, 101510305</p>
     </div>
   );
 }
